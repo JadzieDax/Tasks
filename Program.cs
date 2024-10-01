3d8c8b9cf4a59756d9accb1c2d43cdd3dbb901f6
Dax <timoha.qq@mail.ru>
2024-10-01 22:17:41 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину основания a трапеции: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите длину основания b трапеции: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите высоту h трапеции: ");
+            double h = Convert.ToDouble(Console.ReadLine());
+            double S = Math.Abs(a + b) * h / 2 ;
+            Console.WriteLine("Площадь трапеции: {0:0.00}", S);
+            double P = Math.Sqrt(Math.Pow(Math.Abs(a - b) / 2, 2) + Math.Pow(h, 2)) * 2 + a + b;
+            Console.WriteLine("Периметр трапеции: {0:0.00}", P);
+            Console.ReadLine();
+        }
+    }
+}
